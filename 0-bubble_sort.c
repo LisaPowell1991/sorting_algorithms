7958d6005c2452b11781bf0f31fbad097ab4a310
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-23 09:34:14 -0700

    updated bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,12 +11,15 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int temp, swapped;
+	int temp, is_swapped;
 	size_t pass, current;
+
+	if (array == NULL || size == 0)
+		return;
 
 	for (pass = 0; pass < size - 1; pass++)
 	{
-		swapped = 0;
+		is_swapped = 0;
 
 		for (current = 0; current < size - pass - 1; current++)
 		{
@@ -25,11 +28,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[current];
 				array[current] = array[current + 1];
 				array[current + 1] = temp;
-				swapped = 1;
+				is_swapped = 1;
 				print_array(array, size);
 			}
 		}
-		if (swapped == 0)
+		if (is_swapped == 0)
 			break;
 	}
 }

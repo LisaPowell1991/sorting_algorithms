d470298af0c5e67678177bfc15eab4913e03705b
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-24 07:14:32 -0700

    sort array using selection sort and determine time complexity

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,1 +1,33 @@
 #include "sort.h"
+
+/**
+ * selection_sort - A function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm.
+ * @array: Pointer to an integer array.
+ * @size: The size of the array.
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t current_idx, remaining_idx, min_idx;
+	int temp;
+
+	if (array == NULL || size <= 1)
+		return;
+
+	for (current_idx = 0; current_idx < size - 1; current_idx++)
+	{
+		min_idx = current_idx;
+
+		for (remaining_idx = current_idx + 1; remaining_idx < size; remaining_idx++)
+		{
+			if (array[remaining_idx] < array[min_idx])
+				min_idx = remaining_idx;
+		}
+		temp = array[current_idx];
+		array[current_idx] = array[min_idx];
+		array[min_idx] = temp;
+
+		print_array(array, size);
+	}
+}
